dea74376f555ae716b861dfe167733118ec0f0e5
nitrogendragon <dirtboy4fun@gmail.com>
2019-12-14 17:20:00 -0600

    adjusted movement and made player a scene

--- Pinball/Player/Player.cs
+++ Pinball/Player/Player.cs
@@ -10,7 +10,7 @@ public class Player : KinematicBody2D
     {
         var motion = new Vector2();
         motion.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-        motion.y = Input.GetActionStrength("move_up") - Input.GetActionStrength("move_down");
+        motion.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 
         MoveAndCollide(motion.Normalized() * moveSpeed * delta);
     }
