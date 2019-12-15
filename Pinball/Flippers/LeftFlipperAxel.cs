5347484a83829cc2315ebbaf8802cd8d07e3bf15
nitrogendragon <dirtboy4fun@gmail.com>
2019-12-15 00:11:42 -0600

    Got left and right flipper axel scripts running.

---
+++ Pinball/Flippers/LeftFlipperAxel.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public class LeftFlipperAxel : Node2D
+{
+    [Export] public int RotationSpeed;
+    
+    public int GetRotationSpeed()
+    {
+        return RotationSpeed;
+    }
+
+    public void SetRotationSpeed(int value) => RotationSpeed = value;
+
+    //Checks for key press and rotates if down
+    public bool RotateLeftFlipper()
+    {
+        if(Input.GetActionStrength("move_left_flipper")>0)
+        {
+            return true;
+        }
+        return false;
+    }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        RotationSpeed = -12;
+    }
+
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+ public override void _PhysicsProcess(float delta)
+ {
+     if(RotateLeftFlipper() && RotationDegrees > -20)
+     {
+        Rotation += RotationSpeed * delta;
+     } 
+     else if(RotateLeftFlipper())
+     {
+
+     }
+     else{SetRotation(0);}
+     
+ }
+}
---
+++ Pinball/Flippers/RightFlipperAxel.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public class RightFlipperAxel : Node2D
+{
+    [Export] public int RotationSpeed;
+    
+    public int GetRotationSpeed()
+    {
+        return RotationSpeed;
+    }
+
+    public void SetRotationSpeed(int value) => RotationSpeed = value;
+
+    //Checks for key press and rotates if down
+    public bool RotateRightFlipper()
+    {
+        if(Input.GetActionStrength("move_right_flipper")>0)
+        {
+            return true;
+        }
+        return false;
+    }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        RotationSpeed = 12;
+    }
+
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+ public override void _PhysicsProcess(float delta)
+ {
+     if(RotateRightFlipper() && RotationDegrees < 20)
+     {
+        Rotation += RotationSpeed * delta;
+     } 
+     else if(RotateRightFlipper())
+     {
+         
+     }
+     else{SetRotation(0);}
+     
+ }
+}
